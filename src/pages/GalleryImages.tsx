527096cddb5b8da37fc1ef2642a33780e6f2e84d
eriksfreimanis <eriksfreimanis@squeaky.spawnpoint>
2025-05-13 23:27:54 +0300

    galeries v1.0

--- src/pages/GalleryImages.tsx
+++ src/pages/GalleryImages.tsx
@@ -201,7 +201,7 @@ export default function GalleryImages() {
 
             {/* Images Grid */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {gallery.images.map((image) => (
+              {gallery.images.map((image: GalleryImage) => (
                 <div key={image.id} className="bg-white rounded-lg shadow overflow-hidden">
                   <div className="aspect-w-16 aspect-h-9 bg-gray-200">
                     <img
